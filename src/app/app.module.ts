ac0dd595714a1a90273804d5c1a62fcdb8b1483e
Heber D Pavon <pavonsig@yahoo.com>
2022-02-04 13:12:44 -0500

    - Added ngx-bootstrap, bootstrap package to package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PrintComponent } from './components/print/print.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrintComponent
+    PrintComponent,
+    DropdownComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
